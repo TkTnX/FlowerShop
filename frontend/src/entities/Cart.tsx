b023c69f0d05377ebb4d8c9b0443c12af7498c7d
TkTnX <timur20-09@mail.ru>
2025-12-13 23:32:05 +0500

    done: вёрстка корзины

--- frontend/src/entities/Cart.tsx
+++ frontend/src/entities/Cart.tsx
@@ -1,4 +1,4 @@
-import { Block, type ICart } from "../shared";
+import { Block, Button, type ICart } from "../shared";
 import { CartItem } from "./CartItem";
 
 interface Props {
@@ -9,14 +9,23 @@ export const Cart = ({ cart }: Props) => {
   return (
     <div className="container cart">
       {cart.cartItems.length > 0 ? (
-        <Block>
-          <h3 className="cart__title">Your Cart</h3>
-          <div className="cart__list">
-            {cart.cartItems.map((cartItem) => (
-              <CartItem cartItem={cartItem} key={cartItem.id} />
-            ))}
-          </div>
-        </Block>
+        <>
+          <Block className="cart__left">
+            <h3 className="cart__title">Your Cart</h3>
+            <div className="cart__list">
+              {cart.cartItems.map((cartItem) => (
+                <CartItem cartItem={cartItem} key={cartItem.id} />
+              ))}
+            </div>
+          </Block>
+          <Block className="cart__right">
+            <h5 className="cart__totalPrice">
+              Subtotal for {cart.cartItems.length} items:{" "}
+              <b>{cart.totalPrice}$</b>
+            </h5>
+            <Button className="cart__checkout">Checkout</Button>
+          </Block>
+        </>
       ) : (
         <Block className="cart__empty">
           <img src="/images/cart.svg" />
--- frontend/src/entities/CartItem.tsx
+++ frontend/src/entities/CartItem.tsx
@@ -4,37 +4,42 @@ import type { ICartItem } from "../shared";
 interface Props {
   cartItem: ICartItem;
 }
-
+// TODO: Доделать функционал корзины (добавление в корзину, удаление из корзины, подсчёт total price, изменение quantity)
 export const CartItem = ({ cartItem }: Props) => {
   const product = cartItem.product;
   return (
-    <div className="">
+    <div className="cartItem">
       <img
+        className="cartItem__img"
         src={`${import.meta.env.VITE_PUBLIC_SERVER_URL}${
           product.images[0].image
         }`}
         alt={product.title}
       />
-      <div>
-        <div>
-          <div>
-            <h4>{product.title}</h4>
-            <p>{product.description}</p>
+      <div className="cartItem__info">
+        <div className="cartItem__top">
+          <div className="cartItem__text">
+            <h4 className="cartItem__title">{product.title}</h4>
+            <p className="cartItem__desc">
+              {product.description.slice(0, 100)}...
+            </p>
           </div>
-          <button>
+          <button className="cartItem__delete">
             <Trash />
           </button>
         </div>
-        <div>
-          <div>
-            <p>{product.price}$/unit</p>
-            <div>
+        <div className="cartItem__bottom">
+          <div className="cartItem__price">
+            <p className="cartItem__unit">{product.price}$/unit</p>
+            <div className="cartItem__quantity">
               <button>-</button>
-              <p>{cartItem.quantity}</p>
+              <p className="cartItem__quantity-num">{cartItem.quantity}</p>
               <button>+</button>
             </div>
           </div>
-          <p>Total {product.price * cartItem.quantity}$</p>
+          <p className="cartItem__total">
+            Total {product.price * cartItem.quantity}$
+          </p>
         </div>
       </div>
     </div>
